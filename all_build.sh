c876421aad856dbff9f90bb0d7cfb609781b94bd
ab25cq <ab25cq@icloud.com>
2021-02-19 09:02:08 +0900

    ii

--- all_build.sh
+++ all_build.sh
@@ -1,5 +1,2 @@
-(cd wi; make distclean)
-(cd ayataka; make distclean)
-(cd clover3; make distclean)
 make distclean
-./configure --with-optimize && make && sudo make install && make test && cd clover3 && ./configure --with-optimize  && make && sudo make install && make test && cd ../wi && ./configure --with-optimize && make && sudo make install && cd ../ayataka && ./configure --with-optimize && make && sudo make install
+./configure --with-optimize && make && sudo make install && make test 
