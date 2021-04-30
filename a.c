c8f382dc1ba1c78b9603a09e35f14072abb0028c
ab25cq <ab25cq@icloud.com>
2021-05-01 05:05:43 +0900

    ii

--- a.c
+++ a.c
@@ -1,7 +1,17 @@
-#include <malloc/malloc.h>
+#include <neo-c.h>
+
 
 int main()
 {
-    malloc_zone_t a;
+    list<string>*% li = new list<string>.initialize();
+
+    li.push_back(string("AAA"));
+    li.push_back(string("BBB"));
+    li.push_back(string("CCC"));
+
+    li.sublist(0,1).each {
+        puts(it);
+    }
+
     return 0;
 }
