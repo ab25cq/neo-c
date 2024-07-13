cb72021561927667e3b4715fbd78841984576008
ab25cq <ab25cq@icloud.com>
2024-07-13 00:32:49 +0000

    a.c

--- a.c
+++ a.c
@@ -1,12 +1,24 @@
 #include <neo-c.h>
 
-static __inline unsigned long long __DOUBLE_BITS(double __f)
-{
-    union {double __f; unsigned long long __i;} __u;
-    __u.__f = __f;
-    return __u.__i;
-}
+#define __ISREL_DEF(rel, op, type) \
+static __inline int __is##rel(type __x, type __y) \
+{ return !isunordered(__x,__y) && __x op __y; }
 
+__ISREL_DEF(lessf, <, float_t)
+__ISREL_DEF(less, <, double_t)
+__ISREL_DEF(lessl, <, long double)
+__ISREL_DEF(lessequalf, <=, float_t)
+__ISREL_DEF(lessequal, <=, double_t)
+__ISREL_DEF(lessequall, <=, long double)
+__ISREL_DEF(lessgreaterf, !=, float_t)
+__ISREL_DEF(lessgreater, !=, double_t)
+__ISREL_DEF(lessgreaterl, !=, long double)
+__ISREL_DEF(greaterf, >, float_t)
+__ISREL_DEF(greater, >, double_t)
+__ISREL_DEF(greaterl, >, long double)
+__ISREL_DEF(greaterequalf, >=, float_t)
+__ISREL_DEF(greaterequal, >=, double_t)
+__ISREL_DEF(greaterequall, >=, long double)
 
 int main(int argc, char** argv)
 {
