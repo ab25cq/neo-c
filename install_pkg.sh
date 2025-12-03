693deb82b80ab2590f66f489778de7c0b6c79be5
ab25cq <ab25cq@icloud.com>
2025-12-03 19:13:39 +0900

    ii

--- install_pkg.sh
+++ install_pkg.sh
@@ -2,30 +2,22 @@
 # Mac
 if uname | grep Darwin
 then
-    brew install git pcre readline ncurses make autoconf libgc readline openssl mysql-client mysql
+    brew install git readline ncurses make autoconf libgc readline openssl mysql-client mysql
 # alpine linux
 elif which apk > /dev/null
 then
-    sudo apk add sudo git clang gcc pcre-dev readline-dev ncurses-dev make autoconf valgrind gdb lldb musl-dev gc-dev readline-dev openssl-dev mariadb-connector-c-dev mariadb-client mariadb libdwarf-dev elfutils-dev
+    sudo apk add sudo git clang gcc readline-dev ncurses-dev make autoconf valgrind gdb lldb musl-dev gc-dev readline-dev openssl-dev mariadb-connector-c-dev mariadb-client mariadb libdwarf-dev elfutils-dev
 # termux
 elif test `uname -o` = Android
 then
-    apt install which clang pcre readline ncurses make autoconf valgrind gdb lldb binutils libandroid-glob libgc openssl mariadb libdwarf-dev libelf-dev libc6-dev
-# debian
-elif test -e /etc/debian_version
-then
-    sudo apt install clang gcc libpcre2-dev libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libgc-dev libssl-dev libmariadb-dev-compat libmariadb-dev mariadb-server mariadb-client libdwarf-dev libelf-dev libc6-dev
-# raspberry pi
-elif uname -a | grep raspberry
-then
-    sudo apt install clang gcc libpcre3-dev libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libgc-dev libssl-dev libmariadb-dev-compat libmariadb-dev mariadb-server mariadb-client libdwarf-dev libelf-dev libc6-dev
+    apt install which clang readline ncurses make autoconf valgrind gdb lldb binutils libandroid-glob libgc openssl mariadb libdwarf-dev libelf-dev libc6-dev
 elif which pacman > /dev/null
 then
-    sudo pacman -Syu make valgrind pcre
+    sudo pacman -Syu make valgrind 
 elif which dnf > /dev/null
 then
-    sudo dnf install make valgrind pcre-devel readline-devel ncurses-devel clang gcc which openssl-devel mysql-devel
+    sudo dnf install make valgrind readline-devel ncurses-devel clang gcc which openssl-devel mysql-devel
 elif which apt > /dev/null
 then
-    sudo apt install  clang gcc libpcre3-dev libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libgc-dev libssl-dev libmysqlclient-dev mysql-server libdwarf-dev libelf-dev libc6-dev
+    sudo apt install clang gcc libreadline-dev ncurses-dev make autoconf valgrind gdb lldb libssl-dev libmysqlclient-dev mysql-server 
 fi
