88eb5babceae6a6da68a43616eb6965622450b2c
ab25cq <ab25cq@icloud.com>
2025-12-17 16:40:17 +0900

    iii

--- a.c
+++ a.c
@@ -2,7 +2,6 @@
 
 int main(int argc, char** argv)
 {
-    [1,2,3,4,5].add(6).add(3).add(2).add(10).add(9).sort().map { it.to_string() }.join("\n").to_string().puts();
     
     return 0;
 }
--- code/test_comelang.c
+++ code/test_comelang.c
@@ -28,5 +28,7 @@ int main(int argc, char** argv)
     
     [1,2,3,4,5].add(6).add(3).add(2).add(10).add(9).sort().map { it.to_string() }.join("\n").to_string().puts();
     
+    "ABCDEFGJHI".scan(".").map { it * 5 }.join("\n").puts();
+    
     return 0;
 }
