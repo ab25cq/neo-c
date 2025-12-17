353ce57ddb82da4bc19a2ce848f0e6baa8dd6e60
ab25cq <ab25cq@icloud.com>
2025-12-17 18:19:31 +0900

    iii

--- install_pkg.sh
+++ install_pkg.sh
@@ -2,15 +2,15 @@
 # Mac
 if uname | grep Darwin
 then
-    brew install git readline ncurses make autoconf libgc readline openssl 
+    brew install git readline ncurses make autoconf readline openssl 
 # alpine linux
 elif which apk > /dev/null
 then
-    sudo apk add sudo git clang gcc readline-dev ncurses-dev make autoconf valgrind gdb lldb musl-dev gc-dev readline-dev openssl-dev mariadb-connector-c-dev mariadb-client mariadb libdwarf-dev elfutils-dev
+    sudo apk add sudo git clang gcc readline-dev ncurses-dev make autoconf valgrind gdb lldb musl-dev readline-dev openssl-dev libdwarf-dev elfutils-dev
 # termux
 elif test `uname -o` = Android
 then
-    apt install which clang readline ncurses make autoconf valgrind gdb lldb binutils libandroid-glob libgc openssl mariadb libdwarf-dev libelf-dev libc6-dev
+    apt install which clang readline ncurses make autoconf valgrind gdb lldb binutils libandroid-glob openssl libdwarf-dev libelf-dev libc6-dev
 elif which pacman > /dev/null
 then
     sudo pacman -S make valgrind readline ncurses clang gcc which openssl
