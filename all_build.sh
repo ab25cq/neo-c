f5cea307d32e3e513009f348e55eb4b4ea1528ac
ab25cq <ab25cq@icloud.com>
2025-12-24 20:18:13 +0900

    ii

--- all_build.sh
+++ all_build.sh
@@ -1,6 +1,9 @@
 sh remove_all_build.sh
-echo aaa
+
 if uname -m | grep aarch64
+then
+    (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
+elif uname -a | grep Raspbian
 then
     (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
 else
--- self-host.sh
+++ self-host.sh
@@ -9,6 +9,9 @@ then
 elif uname -a | grep Darwin
 then
     make CFLAGS_OPT="-O2 -g" neo-c -j$(($(sysctl -n hw.logicalcpu) / 2)) && sudo make install
+elif uname -a | grep Raspbian
+then
+    make CFLAGS_OPT="-O2 -g" neo-c && sudo make install
 elif uname -m | grep aarch64
 then
     make CFLAGS_OPT="-O2 -g" neo-c && sudo make install
