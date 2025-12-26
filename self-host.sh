0cdf4b2453030554486353e83a96ccd1e743da6e
ab25cq <ab25cq@icloud.com>
2025-12-27 07:13:33 +0900

    iii

--- self-host.sh
+++ self-host.sh
@@ -5,7 +5,7 @@ export RM="rm -rf"
 
 if uname -a | grep Android
 then
-    make DESTDIR=$HOME CFLAGS_OPT="-O2" neo-c $(($(nproc) / 2)) && make DESTDIR=$HOME install
+    make DESTDIR=$HOME CFLAGS_OPT="-O2" neo-c && make DESTDIR=$HOME install
 elif uname -a | grep Darwin
 then
     make CFLAGS_OPT="-O2 -g" neo-c -j$(($(sysctl -n hw.logicalcpu) / 2)) && sudo make install
