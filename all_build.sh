9086948e3ebc2289847736ec029fd1828a6c3f6e
ab25cq <ab25cq@icloud.com>
2026-02-26 16:10:55 +0900

    ii

--- all_build.sh
+++ all_build.sh
@@ -9,10 +9,12 @@ then
     (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
 elif uname -a | grep Linux
 then
-    make -C code2
-    (echo code && cd code && make test && if [ "${RUN_EXTERNAL_CORPUS:-0}" = "1" ]; then make external-corpus; fi) && (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
+#    make -C code2
+#    (echo code && cd code && make test && if [ "${RUN_EXTERNAL_CORPUS:-0}" = "1" ]; then make external-corpus; fi) && 
+    (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
 else
-    make -C code2
-    make -C code3
-    (echo code && cd code && make test && if [ "${RUN_EXTERNAL_CORPUS:-0}" = "1" ]; then make external-corpus; fi) && (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
+#    make -C code2
+#    make -C code3
+#    (echo code && cd code && make test && if [ "${RUN_EXTERNAL_CORPUS:-0}" = "1" ]; then make external-corpus; fi) && 
+    (echo vin && cd vin && sh fast_build.sh) && (echo zed && cd zed && make && sudo make install) && (echo shsh && cd shsh && make && sudo make install) && (echo mf && cd mf && make && sudo make install)
 fi
