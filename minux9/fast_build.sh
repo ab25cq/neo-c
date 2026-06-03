7a0ec4759df1dbf4d1b51a2e41be62354159a03a
ab25cq <ab25cq@icloud.com>
2026-06-03 09:30:47 +0900

    minux9 force to install qemu

--- minux9/fast_build.sh
+++ minux9/fast_build.sh
@@ -7,9 +7,9 @@ fi
 
 if which apt
 then
-    $SUDO apt-get update
-    $SUDO apt-get install -y gcc-riscv64-linux-gnu \
-                 binutils-riscv64-linux-gnu \
+    sudo apt install gcc-riscv64-linux-elf \
+                 binutils-riscv64-linux-elf \
+                 qemu-system-misc \
                  make \
                  git \
                  gcc \
@@ -23,31 +23,33 @@ if which dnf
 then
     if uname -a | grep x86
     then
-        $SUDO dnf install \
+        sudo dnf install \
             gcc-riscv64-linux-gnu.x86_64 \
+            qemu-system-riscv.x86_64 \
             binutils-riscv64-linux-gnu.x86_64 \
                     make \
                         git \
                         gcc \
                         clang \
                                 gawk \
-                                        vim-common gcc glibc-devel.x86_64
+                                        xxd gcc glibc-devel.x86_64
     else
-        $SUDO dnf install \
+        sudo dnf install \
             gcc-riscv64-linux-gnu.aarch64 \
+            qemu-system-riscv.aarch64 \
             binutils-riscv64-linux-gnu.aarch64 \
                     make \
                         git \
                         gcc \
                         clang \
                                 gawk \
-                                        vim-common gcc glibc-devel.aarch64
+                                        xxd gcc glibc-devel.aarch64
     fi
 fi
 
 if which pacman
 then
-$SUDO pacman -Syu \
+sudo pacman -Syu \
   riscv64-elf-gcc \
     riscv64-elf-binutils \
       make \
@@ -56,14 +58,14 @@ $SUDO pacman -Syu \
             gawk \
             gcc \
             clang \
-                make vim 
+                make vim qemu-system-riscv
                 
 fi
 
 if which apk
 then
-    $SUDO apk add binutils-riscv-none-elf binutils-riscv64 gcc-riscv-none-elf newlib-riscv-none-elf make git gawk gdb-multiarch gcc clang
-    $SUDO apk add spike
+    sudo apk add binutils-riscv-none-elf binutils-riscv64 gcc-riscv-none-elf newlib-riscv-none-elf qemu-riscv64 make git gawk gdb-multiarch qemu-riscv64 qemu-riscv64 qemu-riscv64 qemu-riscv64 qemu-riscv64 qemu-riscv64 qemu-system-riscv64 gcc clang
+    sudo apk add spike
 fi
 
 rm -rf ~/.config/gdb
